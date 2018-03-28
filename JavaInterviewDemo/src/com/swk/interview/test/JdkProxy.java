449291243e53c1fdac58c9db9ba4a221e8f8762f
vicoSong <1663093964@qq.com>
2018-03-28 23:22:42 +0800

    Java面是中常见问题的演示
    主要包含面试中常问的一些代码如：重写equals和hashcode    jdk的动态代理与cglib代理等

---
+++ JavaInterviewDemo/src/com/swk/interview/test/JdkProxy.java
@@ -0,0 +1,36 @@
+/**
+ * @createTime: 2018年3月10日
+ * @copyright: 新智认知数据服务有限公司
+ */
+package com.swk.interview.test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.ByteBuffer;
+
+/**
+ * @classDesc: 类描述: JDK动态代理
+ * @createTime 2018年3月10日 上午8:19:58
+ * @version v1.0.0
+ * @copyright: 新智认知数据服务有限公司
+ */
+public class JdkProxy implements InvocationHandler{
+
+	private Object target;
+	
+	public Object bind(Object target)
+	{
+		this.target=target;
+		return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), this);
+	}
+	
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		method.invoke(proxy, args);
+		return null;
+	}
+	
+	
+}
